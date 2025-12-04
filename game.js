4c7c19fccd99cb35e817f10d17a96fe4f829445b
Callum Shattock <cshattock65@gmail>
2025-12-04 12:00:00 +0000

    Drawing outline of maze before populating

--- game.js
+++ game.js
@@ -18,33 +18,71 @@ function drawlevel(){
         const canvas = document.getElementById("myCanvas");
         const ctx = canvas.getContext("2d");
 
-        let penPointX = 20
-        let penPointY = 20
+        //Draw Maze outline
+        let penPointX = 0
+        let penPointY = 0
+        
+        ctx.beginPath();
+        ctx.moveTo(penPointX, penPointY);
+        penPointX = 1000
+        ctx.lineTo(penPointX, penPointY);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(penPointX, penPointY);
+        penPointY = 1000
+        ctx.lineTo(penPointX, penPointY);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(penPointX, penPointY);
+        penPointX = 0
+        ctx.lineTo(penPointX, penPointY);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(penPointX, penPointY);
+        penPointY = 0
+        ctx.lineTo(penPointX, penPointY);
+        ctx.stroke();
+
         levelArr.forEach(element => {
             if (element === "X"){
                 ctx.beginPath();
                 ctx.moveTo(penPointX, penPointY);
-                penPointX += 50
+                penPointX -= 50
                 penPointY += 0
                 ctx.lineTo(penPointX, penPointY);
                 ctx.stroke();
             }
-            else if(element === "O"){
+            else if(element === "Y"){
                 ctx.beginPath();
                 ctx.moveTo(penPointX, penPointY);
-                penPointX += 20
-                penPointY += 20
+                penPointX += 0
+                penPointY = 50
                 ctx.lineTo(penPointX, penPointY);
-                ctx.stroke();           
+                ctx.stroke();      
+                penPointY += 50     
+                penPointX += 50  
             }
-            else{
+            else if(element === "Y\r\n"){
+                penPointX = 0
                 ctx.beginPath();
                 ctx.moveTo(penPointX, penPointY);
-                penPointX += 20
-                penPointY += 20
+                penPointX += 0
+                penPointY += 50
                 ctx.lineTo(penPointX, penPointY);
-                ctx.stroke();           
+                ctx.stroke(); 
             }
+            else if(element === "-"){
+                ctx.beginPath();
+                ctx.moveTo(penPointX, penPointY);
+                penPointX += 0
+                penPointY += 50
+                ctx.stroke(); 
+                penPointX = 0
+            }
+            
         });
     }
 
